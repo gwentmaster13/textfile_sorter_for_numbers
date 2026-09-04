d8f1fc1c2ce507f9298f40c651f822d94990b56d
Daniel <yepikhindaniel@gmail.com>
2026-09-04 21:00:46 +0200

    Refactored file operations using universal function

--- sorting_the_file.py
+++ sorting_the_file.py
@@ -1,18 +1,22 @@
+def file_open_action(file_name: str, text: str = "", method: str ="a") -> list[str] | None:
+    with open(file_name, method, encoding="UTF-8") as f:
+        if method == "r":
+            return f.readlines()
+        f.write(text)
+        return None
+
 
 def main():
     while True:
         user_file = input("Enter the name of the file: ")
         try:
-            with open(user_file, "r", encoding="utf-8") as f:
-                lines = f.readlines()
-                break
+            lines = file_open_action(file_name=user_file, method= "r")
+            break
         except FileNotFoundError:
-            with open("logg.txt", "a", encoding="utf-8") as logg:
-                logg.write(f"Could not find {user_file} file.")
+            file_open_action(file_name="logg.txt", text= f"Could not find {user_file} file.")
             print(f"Could not find {user_file} file.")
         except IOError:
-            with open("logg.txt", "a", encoding="utf-8") as logg:
-                logg.write(f"Error while getting Input/Output from the {user_file} file.")
+            file_open_action(file_name="logg.txt", text= f"Error while getting Input/Output from the {user_file} file.")
             print("Something went wrong while working with file.")
 
     lines_checked = 0
@@ -24,58 +28,46 @@ def main():
     for number_line, line in enumerate(lines):
         lines_checked += 1
         stripped_line = line.strip().split(" ")
-        print(f"({number_line + 1}) {stripped_line}")
 
         try:
             valid_line = float(stripped_line[0].replace(",", "."))
-            with open("rapport.txt", "a", encoding="utf-8") as rapport:
-                rapport.write(str(valid_line) + "\n")
             validated_lines += 1
             validated_numbers.append(valid_line)
 
         except ValueError:
             if stripped_line[0] == "#" or stripped_line[0] == "":
-                with open("logg.txt", "a", encoding="utf-8") as logg:
-                    logg.write(f'({number_line + 1}) line ignored: "{line.strip()}" \nReason: Commented or empty line\n')
-                    ignored_lines += 1
+                file_open_action(file_name="logg.txt", text= f'({number_line + 1}) line ignored: "{line.strip()}" \n /_Reason: Commented or empty line\n')
+                ignored_lines += 1
             else:
-                with open("logg.txt", "a", encoding="utf-8") as logg:
-                    logg.write(f'({number_line + 1}) line failed to validate: : "{line.strip()}" \nReason: Line consists only of string(s)\n')                
-                    failed_lines += 1
+                file_open_action(file_name="logg.txt", text= f'({number_line + 1}) line failed to validate: : "{line.strip()}" \n /_Reason: Line consists only of string(s)\n')               
+                failed_lines += 1
 
-    with open("rapport.txt", "a", encoding="utf-8") as rapport:
-        rapport.write(f"Valid lines: {validated_lines}\n")
+    file_open_action(file_name="rapport.txt", text= f"Valid lines: {validated_lines}\n")
 
-        if len(validated_numbers) > 0:
-            rapport.write(f"Average value: {sum(validated_numbers) / validated_lines}\n")
-            rapport.write(f"Sum of data: {sum(validated_numbers):.2f}\n")
-            rapport.write(f"Max. value: {max(validated_numbers):.2f}\n")
-            rapport.write(f"Min. value: {min(validated_numbers):.2f}\n")
-        else:
-            rapport.write("No valid data was given to calculate.")
+    if len(validated_numbers) > 0:
+        rapport_text = (
+        f"Average value: {sum(validated_numbers) / validated_lines}\n"
+        f"Sum of data: {sum(validated_numbers):.2f}\n"
+        f"Max. value: {max(validated_numbers):.2f}\n"
+        f"Min. value: {min(validated_numbers):.2f}\n"
+        )
+    else:
+        rapport_text = "No valid data was given to calculate."
 
-    with open("logg.txt", "a", encoding="utf-8") as logg:
-        if len(validated_numbers) > 0:
-            logg.write("=========RESULTS==========\n")
-            logg.write(f"Valid lines: {validated_lines}\n")
-            logg.write(f"Ignored lines: {ignored_lines}\n")
-            logg.write(f"Lines with fails: {failed_lines}\n")
-            logg.write(f"Max. value: {(max(validated_numbers)):.2f}\n")
-            logg.write(f"Min. value: {(min(validated_numbers)):.2f}\n")
-            logg.write(f"Average value: {(sum(validated_numbers) / validated_lines):.2f}\n")
+    file_open_action(file_name= "rapport.txt", text= rapport_text)
 
 
-
-
-
-
-        
-
-
-        
-
-
-
+    if len(validated_numbers) > 0:
+        logg_text = (
+        "=========RESULTS==========\n"
+        f"Valid lines: {validated_lines}\n"
+        f"Ignored lines: {ignored_lines}\n"
+        f"Lines with fails: {failed_lines}\n"
+        f"Max. value: {(max(validated_numbers)):.2f}\n"
+        f"Min. value: {(min(validated_numbers)):.2f}\n"
+        f"Average value: {(sum(validated_numbers) / validated_lines):.2f}\n"
+        )
+        file_open_action(file_name="logg.txt", text= logg_text)
 
     
 if __name__ == "__main__":
